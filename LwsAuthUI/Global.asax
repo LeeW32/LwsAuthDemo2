16ca02fa7a1256a2e6bf40cd2f47666281e0fca6
LeeW32 <40112129+LeeW32@users.noreply.github.com>
2019-02-08 19:59:34 +0000

    Add project files.

---
+++ LwsAuthUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LwsAuthUI.MvcApplication" Language="C#" %>
---
+++ LwsAuthUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LwsAuthUI.Startup))]
+namespace LwsAuthUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
